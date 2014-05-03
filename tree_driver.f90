aed1404c8bcfdb1da1df84b58f334e44cb4d6696
cadangelo <cadangelo@wisc.edu>
2014-05-02 19:03:21 -0500

    added call to dagmcinit

--- tree_driver.f90
+++ tree_driver.f90
@@ -18,8 +18,12 @@ implicit none
 type(node), pointer :: new_node ! next node to be inserted 
 type(node), pointer :: head     ! head of the tree
 
+character(len=80) :: filename
 
 write(*,*) "this is what i like"
 
+filename="nested_vol.h5m"
+
+CALL dagmcinit(filename//char(0),len_trim(filename)) ! setup DAG problem
 
 end program tree_driver
