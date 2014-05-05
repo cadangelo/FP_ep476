5bd73f56e9cb9c09b4c6f157803d3984730c9276
cadangelo <cadangelo@wisc.edu>
2014-05-05 18:23:51 -0500

    the volume data mod defines an allocatable array for the volume centroids

---
+++ volume_data_mod.f90
@@ -0,0 +1,9 @@
+module volume_data_mod
+
+
+implicit none
+
+double precision, allocatable :: volume_centroids(:,:) !allocatable array to
+                                                       !store volume centroids
+
+end module volume_data_mod
