06ac7be7558277458a0ff150c434355ebb36e76c
cadangelo <cadangelo@wisc.edu>
2014-05-14 03:52:31 -0500

    added find_point subroutine from vol_fxn to vol_data mod

--- volume_data_mod.f90
+++ volume_data_mod.f90
@@ -1,9 +1,23 @@
 module volume_data_mod
-
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! This module contains a subroutine that selects the correct point
+! on A's surface according to it's ID number.
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
 
 implicit none
 
-!allocatable array to store volume surface points
 double precision, allocatable :: volume_surfpoints(:,:)
+
+contains
+    subroutine find_point(A_id, x, y, z)
+
+    integer, intent(in) :: A_id ! ID of volume A
+    double precision, intent(out) :: x, y, z ! point on surf. of A
+
+    x=volume_surfpoints(A_id,1)
+    y=volume_surfpoints(A_id,2)
+    z=volume_surfpoints(A_id,3)
+    
+    end subroutine find_point
 
 end module volume_data_mod
